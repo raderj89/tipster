a04942a2748e4750ab64df5da2dc6b3f312642cd
Jared Rader <raderj89@gmail.com>
2014-08-05 09:49:53 -0400

    Admin namespace plus admin signin/signout feature and controller tests - admin model tests - rspec configuration - simple cov to track test coverage

---
+++ spec/controllers/admin_sessions_controller_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe Admin::SessionsController do
+  let(:admin) { create(:admin) }
+
+  describe 'GET #new' do
+    it "renders the login page" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'POST #create' do
+    context "with valid credentials" do 
+      before { post :create, session: { email: admin.email, password: admin.password } }
+
+      it "sets the admin ID in the session" do
+        expect(session[:admin_id]).to eq admin.id
+      end
+
+      it "redirects to admin index upon successful login" do 
+        expect(response).to redirect_to '/admin'
+      end
+
+      it "notifies the user of successful login" do
+        expect(flash[:success]).to match(/^You have logged in successfully./)
+      end
+    end
+
+    context "with invalid credentials" do
+      before { post :create, session: { email: '', password: '' } }
+      
+        it "renders the login page" do
+          expect(response).to render_template(:new)
+        end
+
+        it "flashes a notification of invalid login" do
+          expect(flash[:notice]).to match(/^Incorrect email or password./)
+        end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    before { delete :destroy }
+
+    it "logs out the user, destroying admin ID in the session" do
+      expect(session[:admin]).to be_nil
+    end
+
+    it "notifies the user they have logged out" do
+      expect(flash[:notice]).to match(/^You have logged out successfully./)
+    end
+  end
+end
