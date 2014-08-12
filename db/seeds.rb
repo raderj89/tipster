4d0571a7454ce28bd2f3de387c3f233f7676430c
Tea Ho <hello.teaho@gmail.com>
2014-08-12 13:45:39 -0400

    Fix centering

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 Admin.create!(email: 'admin@smartytip.com', password: 'password')
 
-# 25.times do
-#   Property.create!(address: '165 Central Ave',
-#                    city: Faker::Address.city,
-#                    state: 'NY',
-#                    zip: Faker::AddressUS.zip_code,
-#                    picture: File.new("#{Rails.root}/app/assets/images/Apartments.jpg"))
-# end
+25.times do
+  Property.create!(address: '165 Central Ave',
+                   city: Faker::Address.city,
+                   state: 'NY',
+                   zip: Faker::AddressUS.zip_code,
+                   picture: File.new("#{Rails.root}/app/assets/images/Apartments.jpg"))
+end
