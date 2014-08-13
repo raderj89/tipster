a1b4b5a192004e89f9de51a732f37396db8a736d
Tea Ho <hello.teaho@gmail.com>
2014-08-13 16:04:19 -0400

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,21 +31,6 @@ Property.all.each do |property|
                                 avatar: File.new("#{Rails.root}/app/assets/images/bill.jpeg"),
                                 tip_average: rand(99))
 
-    begin
-      recipient = Stripe::Recipient.create(name: employee.full_name,
-                               type: 'individual',
-                               email: employee.email,
-                               bank_account: {
-                                account_number: '000123456789',
-                                routing_number: '110000000',
-                                country: 'US'} )
-      employee.stripe_id = recipient.id
-      employee.save!
-    rescue Stripe::InvalidRequestError => e
-      puts "Stripe error while creating recipient: #{e.message}"
-      false
-    end
-
     property.property_employees.create!(employee_id: employee.id,
                                         title: TITLES.sample,
                                         suggested_tip: rand(99) )
@@ -59,22 +44,42 @@ Property.all.each do |property|
                         password: 'password',
                         avatar: File.new("#{Rails.root}/app/assets/images/bill.jpeg"),
                         signature: "The #{Faker::Name.last_name} family")
-    begin
-      customer = Stripe::Customer.create(description: user.full_name,
-                                         email: user.email,
-                                         card: { number: 4242424242424242,
-                                                 exp_month: 11,
-                                                 exp_year: 2016,
-                                                 cvc: 123 })
-      user.stripe_id = customer.id
-      user.save!
-    rescue Stripe::CardError => e
-      puts "Stripe error while creating customer: #{e.message}"
-      false
-    end
 
     property.property_tenants.create!(user_id: user.id, unit: "1L")
     user_counter += 1
   end
-
 end
+
+employee = Employee.find(1)
+
+begin
+  recipient = Stripe::Recipient.create(name: employee.full_name,
+                           type: 'individual',
+                           email: employee.email,
+                           bank_account: {
+                            account_number: '000123456789',
+                            routing_number: '110000000',
+                            country: 'US'} )
+  employee.stripe_id = recipient.id
+  employee.save!
+rescue Stripe::InvalidRequestError => e
+  puts "Stripe error while creating recipient: #{e.message}"
+  false
+end
+
+user = User.find(1)
+
+begin
+  customer = Stripe::Customer.create(description: user.full_name,
+                                     email: user.email,
+                                     card: { number: 4242424242424242,
+                                             exp_month: 11,
+                                             exp_year: 2016,
+                                             cvc: 123 })
+  user.stripe_id = customer.id
+  user.save!
+rescue Stripe::CardError => e
+  puts "Stripe error while creating customer: #{e.message}"
+  false
+end
+
